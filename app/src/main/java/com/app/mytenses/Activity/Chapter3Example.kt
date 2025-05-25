c79ef302d39221834d86c6314ce99d62fee2926a
USER <nurulqalbizahrani11@gmail.com>
2025-05-25 14:35:11 +0800

    Commit 5 - Quiz Results + Completed Course

--- app/src/main/java/com/app/mytenses/Activity/Chapter3Example.kt
+++ app/src/main/java/com/app/mytenses/Activity/Chapter3Example.kt
@@ -1,6 +1,9 @@
 package com.app.mytenses.Activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,12 +13,21 @@ import com.app.mytenses.R
 class Chapter3Example : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_chapter3_example)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val prevBtn = findViewById<ImageButton>(R.id.btnBackChapt3)
+        val nextBtn = findViewById<Button>(R.id.btnNextChapt3)
+
+        prevBtn.setOnClickListener {
+            navigateTo(OnBoarding2Activity::class.java)
         }
+
+
+    }
+
+    private fun navigateTo(activityClass: Class<*>) {
+        val intent = Intent(this, activityClass)
+        startActivity(intent)
+        finish()
     }
 }
