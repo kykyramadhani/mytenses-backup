68463d5b79e8dc9e1d37ee61e48d459d98445324
aldaaprisagita <aldadwiaprisagita2015@gmail.com>
2025-05-27 20:17:46 +0800

    Comit 16 Modifie Materi

--- app/src/main/java/com/app/mytenses/Activity/CourseMateriSimplePresentFragment.kt
+++ app/src/main/java/com/app/mytenses/Activity/CourseMateriSimplePresentFragment.kt
@@ -6,6 +6,7 @@ import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.app.mytenses.Activity.Chapter1LessonFragment
 import com.app.mytenses.Activity.Chapter2FormulaFragment
 import com.app.mytenses.Activity.Chapter3ExampleFragment
@@ -24,21 +25,34 @@ class CourseMateriSimplePresent : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Tombol kembali
+        // Tombol kembali: skip CourseRingkasan jika ada di backstack
         view.findViewById<ImageButton>(R.id.backButtonMateriSimplePresent)?.setOnClickListener {
-            requireActivity().supportFragmentManager.popBackStack()
+            val fm = requireActivity().supportFragmentManager
+            val count = fm.backStackEntryCount
+
+            for (i in count - 1 downTo 0) {
+                val entry = fm.getBackStackEntryAt(i)
+                if (entry.name == "CourseRingkasan") {
+                    fm.popBackStack(entry.name, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+                    break
+                }
+            }
+
+            // Pop ke fragment sebelumnya
+            fm.popBackStack()
         }
 
-        // Tab Ringkasan -> Ganti fragment kembali
+        // Tab Ringkasan -> Ganti fragment ke CourseRingkasanSimplePresent
         val tabRingkasan = view.findViewById<TextView>(R.id.tabRingkasan)
         tabRingkasan.setOnClickListener {
             val fragment = CourseRingkasanSimplePresent()
             requireActivity().supportFragmentManager.beginTransaction()
                 .replace(R.id.fragment_container, fragment)
-                .addToBackStack(null)
+//                .addToBackStack("CourseRingkasan") // <--- penting!
                 .commit()
         }
 
+        // Tombol Chapter 1
         val btnChapter1 = view.findViewById<Button>(R.id.btnChapter1)
         btnChapter1.setOnClickListener {
             val fragment = Chapter1LessonFragment()
@@ -48,6 +62,7 @@ class CourseMateriSimplePresent : Fragment() {
                 .commit()
         }
 
+        // Tombol Chapter 2
         val btnChapter2 = view.findViewById<Button>(R.id.btnChapter2)
         btnChapter2.setOnClickListener {
             val fragment = Chapter2FormulaFragment()
@@ -57,6 +72,7 @@ class CourseMateriSimplePresent : Fragment() {
                 .commit()
         }
 
+        // Tombol Chapter 3
         val btnChapter3 = view.findViewById<Button>(R.id.btnChapter3)
         btnChapter3.setOnClickListener {
             val fragment = Chapter3ExampleFragment()
@@ -66,13 +82,13 @@ class CourseMateriSimplePresent : Fragment() {
                 .commit()
         }
 
-        val btnBelajar1 = view.findViewById<Button>(R.id.btnBelajar)
+        val btnBelajar1 = view.findViewById<Button>(R.id.btnBelajar1)
         btnBelajar1.setOnClickListener {
             val fragment = Chapter1LessonFragment()
             requireActivity().supportFragmentManager.beginTransaction()
                 .replace(R.id.fragment_container, fragment)
                 .addToBackStack(null)
                 .commit()
-        }
+            }
     }
 }
