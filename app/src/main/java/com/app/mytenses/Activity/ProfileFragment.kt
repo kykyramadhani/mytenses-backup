7fa00b70cb47899e6dfe8a1d395902fbe19182ef
F1D022094_Rizky Insania Ramadhani <rizkyinsania26@gmail.com>
2025-06-14 19:35:39 +0800

    add navigation to settings

--- app/src/main/java/com/app/mytenses/Activity/ProfileFragment.kt
+++ app/src/main/java/com/app/mytenses/Activity/ProfileFragment.kt
@@ -6,13 +6,35 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.app.mytenses.R
+import com.app.mytenses.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+    ): View {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Set listener untuk settings_button
+        binding.settingsButton.setOnClickListener {
+            // Ganti Fragment ke SettingsFragment
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, SettingFragment())
+                .addToBackStack(null) // Tambahkan ke backstack agar tombol "Back" kembali ke ProfileFragment
+                .commit()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
